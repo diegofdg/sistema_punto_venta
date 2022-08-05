fdf90a6137c4417ff0cfc2717a73807ae37ba7d5
diegofdg <diegofdg@gmail.com>
2022-08-05 16:05:37 -0300

    formulario y reporte familias

---
+++ Sol_PuntoVenta.Negocio/N_Familias.cs
@@ -0,0 +1,32 @@
+﻿using Sol_PuntoVenta.Datos;
+using Sol_PuntoVenta.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sol_PuntoVenta.Negocio
+{
+    public class N_Familias
+    {
+        public static DataTable Listado_fa(string cTexto)
+        {
+            D_Familias Datos = new D_Familias();
+            return Datos.Listado_fa(cTexto);
+        }
+
+        public static string Guardar_fa(int nOpcion, E_Familias oPropiedad)
+        {
+            D_Familias Datos = new D_Familias();
+            return Datos.Guardar_fa(nOpcion, oPropiedad);
+        }
+
+        public static string Eliminar_fa(int Ncodigo)
+        {
+            D_Familias Datos = new D_Familias();
+            return Datos.Eliminar_fa(Ncodigo);
+        }
+    }
+}
